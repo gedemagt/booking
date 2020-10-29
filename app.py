88ada7155503cd0f6a8466b326e934abbbcac663
jesper <gedemagt@gmail.com>
2020-10-29 22:59:00 +0100

    Fixed a few SMTP things.

--- app.py
+++ app.py
@@ -16,19 +16,19 @@ fapp.secret_key = os.getenv('SECRET_KEY', 'This is an INSECURE secret!! DO NOT u
 
 fapp.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DB_PATH
 fapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-fapp.config['MAIL_SERVER'] = 'mail.gandi.net'
-fapp.config['MAIL_PORT'] = 587
+fapp.config['MAIL_SERVER'] = os.getenv("SMTP_SERVER")
+fapp.config['MAIL_PORT'] = int(os.getenv("PORT", 587))
 fapp.config['MAIL_USE_SSL'] = False
 fapp.config['MAIL_USE_TLS'] = True
-fapp.config['MAIL_USERNAME'] = 'noreply@jeshj.com'
+fapp.config['MAIL_USERNAME'] = os.getenv("SMTP_USER")
 fapp.config['MAIL_PASSWORD'] = os.getenv("SMTP_PASS")
-fapp.config['MAIL_DEFAULT_SENDER'] = '"Booking" <noreply@jeshj.com>'
+fapp.config['MAIL_DEFAULT_SENDER'] = '"Booking" <noreply@booking.aarhusklatreklub.dk>'
 
 fapp.config['USER_APP_NAME'] = 'Booking'
 fapp.config['USER_ENABLE_EMAIL'] = True
 fapp.config['USER_ENABLE_USERNAME'] = True
 fapp.config['USER_EMAIL_SENDER_NAME'] = 'Booking'
-fapp.config['USER_EMAIL_SENDER_EMAIL'] = 'noreply@jeshj.com'
+fapp.config['USER_EMAIL_SENDER_EMAIL'] = 'noreply@booking.aarhusklatreklub.dk'
 
 fapp.config['USER_ENABLE_CONFIRM_EMAIL'] = False
 fapp.config['USER_ALLOW_LOGIN_WITHOUT_CONFIRMED_EMAIL'] = True
